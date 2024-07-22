eea6f37023f33703d223858f750ddb4a006f445f
Ian Burke <32346378+imb29@users.noreply.github.com>
2024-07-21 18:33:15 -0700

    Create overtake.lua

---
+++ overtake.lua
@@ -0,0 +1,8 @@
+local msg = ac.OnlineEvent({
+    ac.StructItem.key("overtakeScoreEnd"),
+    Score = ac.StructItem.int64(),
+    Multiplier = ac.StructItem.int32(),
+    Car = ac.StructItem.string(64),
+})
+
+-- msg{ Score = personalBest, Multiplier = comboMeter, Car = ac.getCarName(0) }
